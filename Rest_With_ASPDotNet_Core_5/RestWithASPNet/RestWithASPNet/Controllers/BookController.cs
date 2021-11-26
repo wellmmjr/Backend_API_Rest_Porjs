095a704f7faaf3d53792be13b60a8aa0946dbfa3
Wellington Mendes <wellmmjr@github.com>
2021-11-26 15:51:43 -0300

    Rest_With_ASPDotNet_Core_5 commit
    Project over last step, lasting Docker configuration for full target

---
+++ Rest_With_ASPDotNet_Core_5/RestWithASPNet/RestWithASPNet/Controllers/BookController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNet.Business;
+using RestWithASPNet.Data.VO;
+using RestWithASPNet.Hypermedia.Filters;
+using RestWithASPNet.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNet.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Authorize("Bearer")]
+    [Route("v{version:apiVersion}/api/book")]
+    public class BookController : ControllerBase
+    {
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _bookBusiness;
+
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<BookVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult FindAll()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(BookVO))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult FindById(long id)
+        {
+            var book = _bookBusiness.FindById(id);
+            if (book == null) return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPost()]
+        [ProducesResponseType(200, Type = typeof(BookVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult CreateBook([FromBody] BookVO book)
+        {
+            if (book == null) return NotFound();
+
+            return Ok(_bookBusiness.Create(book));
+        }
+        
+        [HttpPut()]
+        [ProducesResponseType(200, Type = typeof(BookVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult UpdateBook([FromBody] BookVO book)
+        {
+            if (book == null) return NotFound();
+
+            return Ok(_bookBusiness.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Rest_With_ASPDotNet_Core_5/RestWithASPNet/RestWithASPNet/Data/VO/PersonVO.cs
@@ -0,0 +1,30 @@
+﻿using RestWithASPNet.Hypermedia;
+using RestWithASPNet.Hypermedia.Abstract;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNet.Data.VO
+{
+    public class PersonVO : ISupportHyperMedia
+    {
+        [JsonPropertyName("cod")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string SecondNamed { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Address { get; set; }
+
+        //[JsonIgnore]
+        public string Gender { get; set; }
+
+        [JsonPropertyName("active")]
+        public bool Enabled { get; set; }
+
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
